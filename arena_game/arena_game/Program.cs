20e206d67d2e9638f799dee9ccd83381bb1cfeef
Lukáš Jahelka <94187567+JahyLuky@users.noreply.github.com>
2024-07-07 14:07:51 +0200

    Add files via upload

---
+++ arena_game/arena_game/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using objektove_programko.Characters;
+using objektove_programko.Characters.Classes;
+
+namespace arena_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            Character player = menu.Display();
+
+            Dice gameDice = new Dice();
+
+            Character gandalf = new Mage("Gandalf", 100, 20, 10, gameDice, 20, 30);
+
+            Arena arena = new Arena(player, gandalf, gameDice);
+
+            arena.Fight();
+        }
+    }
+}

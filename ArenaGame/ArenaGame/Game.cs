96af06f7a54d0136d9b9f4e527be4c845ac19ac7
Lukáš Jahelka <94187567+JahyLuky@users.noreply.github.com>
2024-07-08 08:40:00 +0200

    Add files via upload

---
+++ ArenaGame/ArenaGame/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace arena_game
+{
+    class Dice
+    {
+        private Random random;
+        private int numberOfSides;
+
+        public Dice()
+        {
+            numberOfSides = 10;
+            random = new Random();
+        }
+
+        public Dice(int numberOfSides)
+        {
+            this.numberOfSides = numberOfSides;
+            random = new Random();
+        }
+
+        public int GetNumberOfSides()
+        {
+            return numberOfSides;
+        }
+
+        public int Roll()
+        {
+            return random.Next(1, numberOfSides + 1);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Dice with {0} sides", numberOfSides);
+        }
+    }
+}
